71eb5593bb3ff3160f67feb1c187dabc9c53af0f
Skender Moglica <moglica.s@gmail.com>
2018-11-19 08:25:34 +0100

    chore: using prettier + esline

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,24 +3,19 @@ module.exports = {
     browser: true,
     commonjs: true,
     es6: true,
-    node: true
+    node: true,
   },
-  plugins: ['prettier'],
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
     parser: 'babel-eslint',
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    indent: ['error', 2],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-    'linebreak-style': ['error', 'unix']
-  }
+  },
 };
