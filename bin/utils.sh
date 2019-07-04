f6eee453044d7bee1cc7c9c3d8f11eb84912f0a1
Skender Moglica <moglica.s@gmail.com>
2019-07-04 08:22:19 +0200

    chore: dockerignore file

--- bin/utils.sh
+++ bin/utils.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+IMAGE_REPOSITORY=wp-template-dev
+IMAGE_TAG=latest
+IMAGE_NAME=${IMAGE_REPOSITORY}:${IMAGE_TAG}
+
 docker_install_dependencies() {
   echo -e "\033[1;44m"Installing fresh dependencies..."\033[0m"
   docker run -it --rm -v $(pwd):/usr/src/app ${IMAGE_NAME} sh -c '
